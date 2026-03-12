749d0d5e874eeb7c5590da1e344f37c8d6a46747
naslnva <13585@holbertonstudents.com>
2026-03-12 14:18:33 +0000

    Fix 8-concat_edges task

--- 8-concat_edges.py
+++ 8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[100:106] + str[0:6])
+print(str[39:66] + str[66] + str[107:111] + str[111] + str[:6])
 
