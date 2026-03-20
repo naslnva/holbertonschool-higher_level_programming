673f89123a7658caae93238ab6a0d0ae32abc0a7
naslnva <13585@holbertonstudents.com>
2026-03-20 19:24:33 +0000

    Task 1: Add private size attribute to Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square with size"""
+
+
+class Square:
+    """Square class with private size attribute"""
+
+    def __init__(self, size):
+        """Initialize square with size"""
+        self.__size = size
