397e010bd13b111f4c4a40a37bf921463e6fc35e
naslnva <13585@holbertonstudents.com>
2026-03-21 11:36:59 +0000

    Fix module documentation for lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that returns list of attributes and methods"""
+
+
 def lookup(obj):
-    """Return list of attributes and methods of object"""
+    """Return list of available attributes and methods of an object"""
     return dir(obj)
