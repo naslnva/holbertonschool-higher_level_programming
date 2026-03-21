532b1078525d73f3fc1551eef0332d6dcb819560
naslnva <13585@holbertonstudents.com>
2026-03-21 12:28:47 +0000

    Fix docstrings

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines a class MyList that inherits from list."""
+"""This module defines a class MyList that inherits from list."""
 
 
 class MyList(list):
-    """Class that inherits from list."""
+    """This class inherits from the built-in list class."""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
+        """This method prints the list in ascending sorted order."""
         print(sorted(self))
