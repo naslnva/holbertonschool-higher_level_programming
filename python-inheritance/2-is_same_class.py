d44cad42b24ecc769c8b2240be2b4e6de8e18231
naslnva <13585@holbertonstudents.com>
2026-03-21 12:46:34 +0000

    Fix PEP8 for is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""This module defines a function that checks if an object is exactly an instance of a specified class."""
+"""Module that checks if an object is exactly
+an instance of a specified class.
+"""
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class, otherwise False."""
+    """Return True if obj is exactly an instance
+    of a_class, otherwise False.
+    """
     return type(obj) is a_class
