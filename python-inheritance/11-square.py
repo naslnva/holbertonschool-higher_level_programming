d5368b7620a131de1248d97d89cd7735041fc8a3
naslnva <13585@holbertonstudents.com>
2026-03-21 14:06:29 +0000

    Fix PEP8 line length for Square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -14,5 +14,6 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return string representation."""
-        return "[Square] {}/{}".format(self._Rectangle__width,
-                                      self._Rectangle__height)
+        return "[Square] {}/{}".format(
+            self._Rectangle__width, self._Rectangle__height
+        )
