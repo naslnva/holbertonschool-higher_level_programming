6af6f5a5703d9432474530ff79b621e83e4e9dc1
naslnva <13585@holbertonstudents.com>
2026-03-24 07:09:18 +0000

    Task 0: Abstract Animal class with Dog and Cat

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract base class for animals"""
+
+    @abstractmethod
+    def sound(self):
+        """Return the sound of the animal"""
+        pass
+
+
+class Dog(Animal):
+    """Dog class inheriting from Animal"""
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """Cat class inheriting from Animal"""
+
+    def sound(self):
+        return "Meow"
