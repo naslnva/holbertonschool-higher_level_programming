8b83da6f7757f8479add8b2add166b4ec4217cd7
naslnva <13585@holbertonstudents.com>
2026-03-24 17:44:36 +0000

    Task 0: Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module that reads a text file and prints it to stdout"""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
+    """Reads a UTF8 text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
---
+++ python-input_output/README.md
@@ -0,0 +1,4 @@
+# Python Input Output
+
+This project contains Python scripts for learning how to read and write files,
+use the with statement, and handle JSON serialization and deserialization.
