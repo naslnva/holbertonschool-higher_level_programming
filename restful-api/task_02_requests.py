d04526e1926e5f720b44cba58a64f7bdc9bb69a2
naslnva <13585@holbertonstudents.com>
2026-03-25 10:23:17 +0000

    Task 02: Consume API with Python requests

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Fetch and process data from JSONPlaceholder API
+"""
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """
+    Fetch posts from API and print their titles
+    """
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post.get("title"))
+
+
+def fetch_and_save_posts():
+    """
+    Fetch posts from API and save them into CSV
+    """
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        data = [
+            {
+                "id": post.get("id"),
+                "title": post.get("title"),
+                "body": post.get("body")
+            }
+            for post in posts
+        ]
+
+        with open("posts.csv", "w", newline="") as file:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(data)
