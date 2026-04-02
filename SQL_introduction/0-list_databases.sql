acbd762ff56c18ee24eed97e14d5edf7b3424857
naslnva <13585@holbertonstudents.com>
2026-04-02 15:53:36 +0000

    Add SQL_introduction folder with 0-list_databases.sql and README.md

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- 0. List databases
+-- This script lists all databases in MySQL server
+SHOW DATABASES;
---
+++ SQL_introduction/README.md
@@ -0,0 +1,3 @@
+# SQL Introduction
+
+This project contains SQL scripts to practice basic MySQL commands.
