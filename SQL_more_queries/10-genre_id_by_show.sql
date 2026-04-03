2e79089974e3a5fdd0bf56ac6ec617220c5fc036
naslnva <13585@holbertonstudents.com>
2026-04-03 19:00:09 +0000

    Add 10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- List all TV shows that have at least one genre linked with genre_id
+-- Sort by tv_shows.title and tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
