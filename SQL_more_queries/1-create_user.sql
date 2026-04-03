b41be47999f756ecbcf6a1bc1c9e8b6af6b9a83b
naslnva <13585@holbertonstudents.com>
2026-04-03 16:51:47 +0000

    Add 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Create user user_0d_1 with all privileges
+-- Creates the MySQL server user user_0d_1 with all privileges
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
