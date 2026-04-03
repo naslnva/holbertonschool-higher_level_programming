adaf7d529659813bf234e35eb5aa14c530e76a4c
naslnva <13585@holbertonstudents.com>
2026-04-03 16:59:44 +0000

    Add 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
--- Create user user_0d_1 with all privileges and a password
+-- Create user user_0d_1 with all privileges on the MySQL server
 
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
