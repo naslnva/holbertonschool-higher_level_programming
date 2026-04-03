695b2ea809450495ef2e19b94617417b77e05f90
naslnva <13585@holbertonstudents.com>
2026-04-03 19:28:28 +0000

    Add 13-count_shows_by_genre.sql

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- 13-count_shows_by_genre.sql
+SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
+FROM genres g
+JOIN tv_show_genres tg ON g.id = tg.genre_id
+GROUP BY g.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
