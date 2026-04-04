c09c7ac82e77d7296abcb7529979f8d275d396ab
naslnva <13585@holbertonstudents.com>
2026-04-04 15:47:59 +0000

    Add 13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,15 @@
--- 13-count_shows_by_genre.sql
-SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
-FROM genres g
-JOIN tv_show_genres tg ON g.id = tg.genre_id
-GROUP BY g.id
+-- Task 13: Number of shows by genre
+-- List all genres and the number of shows linked to each
+-- Columns: genre, number_of_shows
+-- Only display genres that have at least one show
+-- Results sorted descending by number_of_shows
+-- Only one SELECT statement
+
+SELECT tv_genres.name AS genre,
+       COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
