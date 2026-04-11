2677d0e86267f1db18184f379f4df13f8b29bd9f
naslnva <13585@holbertonstudents.com>
2026-04-11 18:41:32 +0000

    Get state by name using SQLAlchemy safely

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Get State by name safely"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_name = sys.argv[4]
+
+    state = session.query(State)\
+        .filter(State.name == state_name)\
+        .first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
