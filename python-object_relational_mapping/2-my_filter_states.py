b5f992bc65e74b6e233c70656c1da20251fa9534
naslnva <13585@holbertonstudents.com>
2026-04-11 17:15:42 +0000

    fix pycodestyle line length

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists states that match user input
+Lists states matching user input
 """
 
 import MySQLdb
@@ -8,6 +8,7 @@ import sys
 
 
 if __name__ == "__main__":
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -21,8 +22,8 @@ if __name__ == "__main__":
 
     query = (
         "SELECT * FROM states WHERE name = '{}' "
-        "ORDER BY states.id ASC".format(sys.argv[4])
-    )
+        "ORDER BY states.id ASC"
+    ).format(sys.argv[4])
 
     cur.execute(query)
 
